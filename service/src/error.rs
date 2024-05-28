818c56b7b2ec85b1b2a4cb349ac0aadb6b3fe6ff
Mücahit Kurtlar <mucahit@tutanota.com>
2024-05-28 03:26:18 +0300

    feat: Error handling

--- service/src/error.rs
+++ service/src/error.rs
@@ -5,6 +5,7 @@ pub type Result<T> = core::result::Result<T, Error>;
 
 #[derive(Serialize, Deserialize)]
 pub struct ErrorResponse {
+    pub code: u16,
     pub error: String,
     pub details: Option<String>,
 }
@@ -25,10 +26,12 @@ impl IntoErrorResponse for Error {
     fn into_error_response(self) -> ErrorResponse {
         match self {
             Error::InternalError(e) => ErrorResponse {
+                code: 500,
                 error: "Sunucu hatası".to_string(),
                 details: Some(e.to_string()),
             },
             Error::InvalidRequest(e) => ErrorResponse {
+                code: 400,
                 error: "Geçersiz istek".to_string(),
                 details: Some(e.to_string()),
             },
