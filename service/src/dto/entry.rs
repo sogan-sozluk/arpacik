b2d8c0689b2f7742f87f2f10aa07c41ff682a5f9
Mücahit Kurtlar <mucahit@tutanota.com>
2024-05-31 01:48:12 +0300

    feat: Entry actions

--- service/src/dto/entry.rs
+++ service/src/dto/entry.rs
@@ -1,14 +1,6 @@
 use serde::{Deserialize, Serialize};
 use validator::Validate;
 
-#[derive(Debug, Serialize, Deserialize, Validate)]
-pub struct CreateEntryRequest {
-    #[validate(length(min = 1, max = 75))]
-    pub title: String,
-    #[validate(length(min = 1, max = 65535))]
-    pub content: String,
-}
-
 #[derive(Debug, Serialize, Deserialize)]
 pub struct EntryDto {
     pub id: i32,
@@ -19,4 +11,18 @@ pub struct EntryDto {
     pub user_nickname: String,
     pub created_at: String,
     pub updated_at: String,
+}
+
+#[derive(Debug, Serialize, Deserialize, Validate)]
+pub struct CreateEntryRequest {
+    #[validate(length(min = 1, max = 75))]
+    pub title: String,
+    #[validate(length(min = 1, max = 65535))]
+    pub content: String,
+}
+
+#[derive(Debug, Serialize, Deserialize, Validate)]
+pub struct UpdateEntryRequest {
+    #[validate(length(min = 1, max = 65535))]
+    pub content: String,
 }
